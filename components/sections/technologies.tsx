0494cd9cb3441c4362cefff0b6b5ba0034911be0
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-11-23 10:54:54 -0300

    add technologies

--- components/sections/technologies.tsx
+++ components/sections/technologies.tsx
@@ -25,22 +25,24 @@ export function Technologies() {
         'visual',
         'vite',
         'vitest',
+        'docker',
+        'insomia',
     ];
 
     return (
         <section id="tecnologias">
-            <div className="container py-16 md:py-20 mx-auto px-4">
+            <div className="container px-4 py-16 mx-auto md:py-20">
                 <div className="w-full mx-auto prose text-left max-w-prose">
-                    <h2 className="flex items-center gap-4 mb-8 md:mb-10 text-3xl md:text-4xl font-black font-heading dark:text-white">
+                    <h2 className="flex items-center gap-4 mb-8 text-3xl font-black md:mb-10 md:text-4xl font-heading dark:text-white">
                         Tecnologias ✨
                     </h2>
-                    <p className="mb-8 text-lg md:text-xl leading-relaxed text-muted-foreground">
+                    <p className="mb-8 text-lg leading-relaxed md:text-xl text-muted-foreground">
                         Navegar pelo vasto mundo da programação me deu a oportunidade de trabalhar
                         com diversas tecnologias e ferramentas. Abaixo estão algumas das principais
                         tecnologias que fazem parte do meu conjunto de habilidades:
                     </p>
                 </div>
-                <div className="flex flex-wrap items-center justify-center w-full md:w-2/3 gap-4 mx-auto">
+                <div className="flex flex-wrap items-center justify-center w-full gap-4 mx-auto md:w-2/3">
                     {technologies.map((tech, key) => (
                         <TooltipProvider key={key}>
                             <Tooltip>
