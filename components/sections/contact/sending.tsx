4027cb85b2f9701f663d46faa533156e4e52aa4e
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-11-27 16:27:48 -0300

    Initial commit

--- components/sections/contact/sending.tsx
+++
@@ -1,7 +0,0 @@
-export function ContactSending() {
-    return (
-        <div className="p-4 mt-12 mx-auto border border-dashed rounded border-main bg-main/5 max-w-prose text-muted-foreground">
-            <h4>🚀 Aguarde um pouquinho, sua mensagem está voando para minha caixa de entrada.</h4>
-        </div>
-    );
-}
--- components/sections/separator.tsx
+++
@@ -1,9 +0,0 @@
-export function Separator() {
-    return (
-        <div className="container flex items-center justify-center gap-12 mx-auto opacity-5 dark:opacity-10">
-            <div className="flex-1 h-0.5 bg-foreground rounded-full"></div>
-            <div className="flex-grow-0 w-8 h-8 rotate-45 border-2 border-foreground"></div>
-            <div className="flex-1 h-0.5 bg-foreground rounded-full"></div>
-        </div>
-    );
-}
